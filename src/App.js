2488e5366884ca7fbbf8ca1aece66101e290b37e
donkey103 <donkey103@naver.com>
2023-12-08 11:56:18 +0900

    Link 태그 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 import MainPageComponent from "./main";
+import UploadPage from "./upload";
+import ProudctPage from "./product";
+import { Routes, Route } from "react-router-dom";
 function App() {
-  return <MainPageComponent />;
+  return (
+    <div>
+      <Routes>
+        <Route exact={true} path="/" element={<MainPageComponent />} />
+        <Route exact={true} path="/products/:id" element={<ProudctPage />} />
+        <Route exact={true} path="/upload" element={<UploadPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
