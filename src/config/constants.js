bd465ca6f76c3225690964528b8db55834d939aa
donkey103 <donkey103@naver.com>
2023-12-16 18:17:37 +0900

    api url 주소 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,4 @@
-export const API_URL = "http://localhost:8080";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://grab-market-server-test-0013.fly.dev"
+    : "http://localhost:8080";
