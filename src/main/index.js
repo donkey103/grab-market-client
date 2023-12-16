bec7c21a2e65bd736c7998f243df965bb3e4f5c3
donkey103 <donkey103@naver.com>
2023-12-16 15:29:03 +0900

    그랩 마켓 완료

--- src/main/index.js
+++ src/main/index.js
@@ -2,14 +2,18 @@ import React from "react";
 import "./index.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { API_URL } from "../config/constants.js";
+import { Carousel } from "antd";
+dayjs.extend(relativeTime);
 
 function MainPage() {
   const [products, setProducts] = React.useState([]);
+  const [banners, setBanners] = React.useState([]);
   React.useEffect(function () {
     axios
-      .get(
-        "https://772e1e35-a0a8-4061-b51d-fd3165173dba.mock.pstmn.io/products"
-      )
+      .get(`${API_URL}/products`)
       .then(function (result) {
         const products = result.data.products;
         setProducts(products);
@@ -17,32 +21,59 @@ function MainPage() {
       .catch(function (error) {
         console.error("에러 발생 : ", error);
       });
+
+    axios
+      .get(`${API_URL}/banners`)
+      .then((result) => {
+        const banners = result.data.banners;
+        setBanners(banners);
+      })
+      .catch((error) => {
+        console.error("에러 발생 : ", error);
+      });
   }, []);
 
   return (
     <div>
-      <div id="banner">
-        <img src="images/banners/banner1.png" alt="" />
-      </div>
-      <h1>판매되는 상품들</h1>
+      <Carousel autoplay autoplaySpeed={3000}>
+        {banners.map((banner, index) => {
+          return (
+            <Link to={banner.href}>
+              <div id="banner">
+                <img src={`${API_URL}/${banner.imageUrl}`} alt="" />
+              </div>
+            </Link>
+          );
+        })}
+      </Carousel>
+      <h1 id="product-headline">판매되는 상품들</h1>
       <div id="product-list">
         {products.map(function (product, index) {
           return (
             // key는 map 메서드 사용시 경고 발생 방지용으로 넣음(강의 외 내용)
             <div className="product-card" key={product.id}>
+              {product.soldout === 1 && <div className="product-blur" />}
               <Link className="product-link" to={`/products/${product.id}`}>
                 <div>
-                  <img className="product-img" src={product.imageUrl} />
+                  <img
+                    className="product-img"
+                    src={`${API_URL}/${product.imageUrl}`}
+                  />
                 </div>
                 <div className="product-contents">
                   <span className="product-name">{product.name}</span>
                   <span className="product-price">{product.price}원</span>
-                  <div className="product-seller">
-                    <img
-                      className="product-avatar"
-                      src="images/icons/avatar.png"
-                    />
-                    <span>{product.seller}</span>
+                  <div className="product-footer">
+                    <div className="product-seller">
+                      <img
+                        className="product-avatar"
+                        src="images/icons/avatar.png"
+                      />
+                      <span>{product.seller}</span>
+                    </div>
+                    <span className="product-date">
+                      {dayjs(product.createdAt).fromNow()}
+                    </span>
                   </div>
                 </div>
               </Link>
